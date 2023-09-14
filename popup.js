06ee53769f9888b48ec740dc4481fb2597f636dd
MilianDevOps <miliandevops@gmail.com>
2023-09-14 16:59:48 -0400

    Change Name add Popup

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Popup DOM fully loaded and parsed');
+
+    // Here you might add event listeners for any buttons or links that you might add in the future.
+    // For example:
+    // document.querySelector('#someButton').addEventListener('click', () => {
+    //     console.log('Button clicked!');
+    // });
+});
